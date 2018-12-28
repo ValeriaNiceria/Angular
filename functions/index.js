2c1e5f821fde601df495ed8e1fc630efcafe5a10
ValeriaNiceria <valerianiceria@gmail.com>
2018-12-28 16:26:38 -0200

    function - send e delete

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.onFileSend = functions.storage.object().onFinalize(event => {
+	const object = event.data
+	const bucket = object.bucket
+	const contentType = object.contentType
+	const filePath = object.name
+	console.log('File send detected, function execution started')
+	return
+})
+
+
+exports.onFileDelete = functions.storage.object().onDelete(event => {
+	const object = event.data
+	const bucket = object.bucket
+	const contentType = object.contentType
+	const filePath = object.name
+	console.log('File delete detected, function execution started')
+	return
+})
