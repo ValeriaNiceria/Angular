150d66dac3f578bbef85e9f0501aebe40efcecbc
ValeriaNiceria <valerianiceria@gmail.com>
2018-12-28 22:57:06 -0200

    Criando o component 'tentativas' e 'progresso' via CLI

--- projetos/app1/src/app/app.module.ts
+++ projetos/app1/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 
 import { TopoComponent } from './topo/topo.component';
 import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    PainelComponent
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule,
