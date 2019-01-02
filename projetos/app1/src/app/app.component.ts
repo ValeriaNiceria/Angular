5a8deed98d2f75264855b83f2fa60aae3498a733
ValeriaNiceria <valerianiceria@gmail.com>
2019-01-01 23:31:48 -0200

    Controlando a exibição do painel e testando o metodo ngOnDestroy

--- projetos/app1/src/app/app.component.ts
+++ projetos/app1/src/app/app.component.ts
@@ -7,11 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+	public jogoEmAndamento: boolean = true
+	public mensagemEncerramento: string
+
 	public encerrarJogo(tipo: string): void {
 		if (tipo == 'vitoria') {
-
+			this.mensagemEncerramento = 'Fim de jogo, você ganhou :D'
 		} else {
-			
+			this.mensagemEncerramento = 'Fim de jogo, infelizmente você perdeu :('
 		}
+
+		this.jogoEmAndamento = false
 	}
 }
