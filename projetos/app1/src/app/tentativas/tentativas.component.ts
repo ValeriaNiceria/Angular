8d3c709c474e5ea9e32a3f3ebab69ec1e73a0633
ValeriaNiceria <valerianiceria@gmail.com>
2019-01-01 22:31:02 -0200

    Controlando as tentativas

--- projetos/app1/src/app/tentativas/tentativas.component.ts
+++ projetos/app1/src/app/tentativas/tentativas.component.ts
@@ -24,7 +24,10 @@ export class TentativasComponent implements OnInit, OnChanges {
 	}
 
 	ngOnChanges() {
-		console.log('Tentativas: ', this.tentativas)
+		if (this.tentativas != this.coracoes.length) {
+			let indice = this.coracoes.length - this.tentativas
+			this.coracoes[indice - 1].cheio = false
+		}
 	}
 
 }
