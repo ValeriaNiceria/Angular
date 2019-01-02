9d0e33c421b4ff0c62a7a5a2ab81ff77652c8879
ValeriaNiceria <valerianiceria@gmail.com>
2019-01-02 10:14:44 -0200

    Contato service

--- projetos/crud-firebase/src/app/contatos/shared/contato.service.ts
+++ projetos/crud-firebase/src/app/contatos/shared/contato.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Contato } from './contato'
+import { AngularFireDatabase } from '@angular/fire/database'
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContatoService {
 
-  constructor() { }
+	constructor(
+		private db: AngularFireDatabase
+	) { }
+
+	public insert(contato: Contato) {
+		this.db.list('contato').push(contato)
+			.then((result: any) => {
+				console.log(result.key)
+			})
+	}
+
+	public update(contato: Contato, key: string) {
+		this.db.list('contato').update(key, contato)
+			.catch((error) => {
+				console.error('Erro ao atualizar: ', error)
+			})
+	}
+
+	public getAll() {
+		return this.db.list('contato')
+			.snapshotChanges()
+			.pipe(
+				map(changes => {
+					return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+				})
+			)
+	}
+
+	public delete(key: string) {
+		this.db.object(`contato/${key}`).remove()
+	}
 }
