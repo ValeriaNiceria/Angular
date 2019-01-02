2b35999452aade8a81c2ec9c60f743e785290955
ValeriaNiceria <valerianiceria@gmail.com>
2019-01-02 09:46:13 -0200

    Importando modulos angularfire app module

--- projetos/crud-firebase/src/app/app.module.ts
+++ projetos/crud-firebase/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+
+// Firebase
+import { environment } from '../environments/environment'
+import { AngularFireModule } from '@angular/fire'
+import { AngularFireDatabaseModule } from '@angular/fire/database'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
